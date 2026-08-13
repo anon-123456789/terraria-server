1eef226c140f9cb35a4f3a97a24cfc1157fc1449
Anon <88592342+anon-123456789@users.noreply.github.com>
2026-08-13 13:12:31 +0800

    handle SIGTERM properly (probably)

--- containers/vanilla/1.4.5.6/run-vanilla.sh
+++ containers/vanilla/1.4.5.6/run-vanilla.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
+SERVER_FIFO=$(mktemp)
+
+stop() {
+    echo "SIGTERM requested, Sending exit command to server..."
+    echo -e "\nexit" > "${SERVER_FIFO}"
+}
+
 CMD="./TerrariaServer -x64 -config /config/serverconfig.txt -banlist /config/banlist.txt"
 
 # Create default config files if they don't exist
@@ -27,5 +34,16 @@ if [ "${world:-null}" != null ]; then
     CMD="$CMD -world /config/$world"
 fi
 
+# Create a fifo that we can write to send commands to the server
+mkfifo "${SERVER_FIFO}"
+echo "Server fifo is set to ${SERVER_FIFO}"
+
+# Docker sends us a SIGTERM when the container stops, so trap it to actually shutdown the server cleanly
+echo "Trapping SIGTERM..."
+trap "stop" SIGTERM
+
 echo "Starting container, CMD: $CMD $@"
-exec $CMD $@
+exec $CMD $@ < <(tail -f "${SERVER_FIFO}" & cat /dev/tty)
+
+echo "Removing server fifo ${SERVER_FIFO}"
+rm -f "${SERVER_FIFO}"
